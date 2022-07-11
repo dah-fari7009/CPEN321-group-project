6489b9d81ccb048d8f3edc342ac3084b80510a7c
kevindeng <2391217461@qq.com>
2022-07-10 21:15:58 -0700

    Add routes for api call

---
+++ app/src/main/java/com/example/eloquent/router.java
@@ -0,0 +1,30 @@
+package com.example.eloquent;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+public class router {
+
+//    private void createUser(String id) {
+//        User user = User.getInstance();
+//        StringRequest stringRequest = new StringRequest(Request.Method.GET, "http://20.104.77.70:8081/", new Response.Listener<String>() {
+//            @Override
+//            public void onResponse(String response) {
+//
+//
+//            }
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                Toast.makeText(getApplicationContext(), error.toString().trim(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        requestQueue.add(stringRequest);
+//    }
+}
