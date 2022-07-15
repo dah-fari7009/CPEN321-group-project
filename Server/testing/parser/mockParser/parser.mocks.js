45dc72b16752c92e29dda927d7192e76e679db76
Ubuntu <azureuser@Eloquent.rdpwlew5zbtedbsiva3qklkmgg.ux.internal.cloudapp.net>
2022-07-15 04:18:44 +0000

    Mock unParsePresentation() from export.js

--- Server/testing/parser/mockParser/parser.mocks.js
+++ Server/testing/parser/mockParser/parser.mocks.js
@@ -3,6 +3,60 @@
 const validParseText = "\\begin{presentation} \\title: hello \\end{presentation}";
 const invalidParseText = "\\begin{presentation} \\title: hello";
 
+const sampleUnparseInput = {
+    "title": "this is the title",
+    "cards": [{
+        "backgroundColor": 1,
+        "transitionPhrase": "transition phrase",
+        "endWithPause": 0,
+        "front": {
+            "backgroundColor": 2,
+            "content": {
+                "font": "front font",
+                "style": "front style",
+                "size": 3,
+                "colour": 4,
+                "message": "> front message"
+            }
+        },
+        "back": {
+            "backgroundColor": 5,
+            "content": {
+                "font": "back font",
+                "style": "back style",
+                "size": 6,
+                "colour": 7,
+                "message": "> back message \n> point 2"
+            }
+        }
+    },
+    {
+        "backgroundColor": 1,
+        "transitionPhrase": "w",
+        "endWithPause": 0,
+        "front": {
+            "backgroundColor": 2,
+            "content": {
+                "font": "d",
+                "style": "d",
+                "size": 4,
+                "colour": 5,
+                "message": "d"
+            }
+        },
+        "back": {
+            "backgroundColor": 5,
+            "content": {
+                "font": "a",
+                "style": "a",
+                "size": 6,
+                "colour": 7,
+                "message": "a"
+            }
+        }
+    }]
+}; 
+
 const mockParse = jest.fn((req, res) => {
 	return new Promise ((resolve, reject) => {
 		var userID = req.body.userID;
@@ -15,10 +69,18 @@ const mockParse = jest.fn((req, res) => {
 	
 }); 
 
-// const mockUnParsePresentation = jest.fn();
+const mockUnParsePresentation = jest.fn((req, res) => {
+	var presObj = req.body;
+	if (presObj == sampleUnparseInput) {
+		return "\\begin{presentation}\n\t\\title this is the title\n\t\\begin{cuecard}[backgroundColor=1, transitionPhrase=transition phrase, endWithPause=0]\n\t\t\\begin{point}[backgroundColor=2, font=front font, style=front style, size=3, colour=4]\n\t\t\t\\item front message\n\t\t\\end{point}\n\t\t\\begin{details}[backgroundColor=5, font=back font, style=back style, size=6, colour=7]\n\t\t\t\\item back message\n\t\t\t\\item point 2\n\t\t\\end{details}\n\t\\end{cuecard}\n\t\\begin{cuecard}[backgroundColor=1, transitionPhrase=w, endWithPause=0]\n\t\t\\begin{point}[backgroundColor=2, font=d, style=d, size=4, colour=5]\n\t\t\t\\item\n\t\t\\end{point}\n\t\t\\begin{details}[backgroundColor=5, font=a, style=a, size=6, colour=7]\n\t\t\t\\item\n\t\t\\end{details}\n\t\\end{cuecard}\n\\end{presentation}";
+	}
+
+});
 
 module.exports = {
 	validParseText,
 	invalidParseText,
-	mockParse
+	sampleUnparseInput,
+	mockParse,
+	mockUnParsePresentation
 }
