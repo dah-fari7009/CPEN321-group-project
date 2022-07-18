575f84115da7bb1275806a34badb5b2ae1c31029
Zachary Choo <zachary.h.choo@gmail.com>
2022-07-17 17:16:43 -0700

    idk

--- Server/testing/presManager/presManager.test.js
+++ Server/testing/presManager/presManager.test.js
@@ -1,1 +1,7 @@
 const presManager = require("../../presManager/presManager");
+
+test('tries to get a presentation that doesnt exist', () => {
+    return presManager.getPresTitle(1, 2).then((data) => {
+        expect(data).toBe([]); 
+    });
+});
