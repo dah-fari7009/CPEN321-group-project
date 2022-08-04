87a145cdb0bbee2ebb1853011949465532c04246
kevindeng <2391217461@qq.com>
2022-08-03 22:57:24 -0700

    Added importTest for presentation

---
+++ frontend/app/src/androidTest/java/com/example/eloquent/ImportTest.java
@@ -0,0 +1,91 @@
+package com.example.eloquent;
+
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class ImportTest {
+
+    @Rule
+    public ActivityScenarioRule<Login> mActivityScenarioRule =
+            new ActivityScenarioRule<>(Login.class);
+
+    @Test
+    public void test() {
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.LB), withText("Dev Login"),
+                        childAtPosition(
+                                allOf(withId(R.id.loginButtons),
+                                        childAtPosition(
+                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                                2)),
+                                0),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+
+    static Intent intent;
+    static {
+        intent = new Intent(ApplicationProvider.getApplicationContext(), MainActivity.class);
+        Bundle bundle = new Bundle();
+        String presentation = String.valueOf(R.string.sample_import_input);
+        bundle.putString("presentation", presentation);
+        intent.putExtras(bundle);
+    }
+
+    public void DisplayedImportedPresentation() {
+
+        // Assert that the data we set up above is shown.
+        onData(withId(R.id.listOfPres)).atPosition(0)
+                .check(matches(hasDescendant(withText("Sample Presentation"))));
+    }
+}
