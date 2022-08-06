c4dfacbad234867ec7b5a320ce4035d251d1910a
RickyF <72296783+fakeapocrypha@users.noreply.github.com>
2022-08-05 17:49:02 -0700

    Update wsserver.js

--- backend/wsserver.js
+++ backend/wsserver.js
@@ -20,27 +20,7 @@ var mongooseConnect = async function () {
 
 }
 mongooseConnect();
-var position;
-var PID;
-var userID;
-var a;
-var cueCards_num;
-var newCard;
-var tmpcueCard;
-var tmpFrontHistory;
-var tmpBackHistory;
-var tmpFrontPosition;
-var tmpBackPosition;
-var cardFace;
-var userID;
-var before_text;
-var start;
-var end;
-var diff;
-var undoEnd;
-var undoHistory;
-var length;
-var redoHistory;
+
 
 
 class History {
@@ -75,6 +55,27 @@ wss.getUniqueID = function () {
 
 
 wss.on("connection",(ws) => {
+    var position;
+    var PID;
+    var userID;
+    var a;
+    var cueCards_num;
+    var newCard;
+    var tmpcueCard;
+    var tmpFrontHistory;
+    var tmpBackHistory;
+    var tmpFrontPosition;
+    var tmpBackPosition;
+    var cardFace;
+    var userID;
+    var before_text;
+    var start;
+    var end;
+    var diff;
+    var undoEnd;
+    var undoHistory;
+    var length;
+    var redoHistory;
     console.log('server:A client logged in');
     ws.id = wss.getUniqueID();
 
